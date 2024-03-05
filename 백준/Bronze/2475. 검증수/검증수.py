70aece50fb8c1946a762466a7c6f2d61373b123c
Daniel Kim <157444165+kingboseong@users.noreply.github.com>
2024-03-05 15:21:07 +0900

    Update 검증수.py

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -2,3 +2,13 @@ a,b,c,d,e = input().split()
 a,b,c,d,e = int(a), int(b), int(c), int(d), int(e)
 
 print(((a*a)+(b*b)+(c*c)+(d*d)+(e*e))%10)
+
+a = int(input())
+
+for i in range(1, a+1):
+    print(" "*(a-i), "*"*i)
+
+a = int(input())
+
+for i in range(1, a+1):
+    print(("*"*i).rjust(a))
